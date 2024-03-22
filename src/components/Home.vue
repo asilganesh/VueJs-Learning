301107bde608851081e49782713683c2786881d1
ganes asil <ganeshasil464@gmail.com>
2024-03-22 12:34:35 +0530

    home

---
+++ src/components/Home.vue
@@ -0,0 +1,38 @@
+<template>
+
+<h1>Email: {{ getData().email }}</h1>
+<h1>name: {{ getName() }}</h1>
+<h1>Name : {{ getData().lastname }}</h1>
+</template>
+
+<script>
+
+export default {
+    name:"HomeComponent",
+   data(){
+    return{
+        email:"ganesh@gmail.com",
+      
+    }
+   },
+   methods:{
+    getName:function(){
+        return "Ganesh asil"
+    },
+    getData:function(){
+        return{
+            fname:"Ganesh",
+            lastname:"Asil",
+            email:this.email
+        }
+    }
+   }
+}
+</script>
+
+<style scoped>
+h1{
+    color:red
+}
+
+</style>
