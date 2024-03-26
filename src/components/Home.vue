5cce6181e49ba9f45a07ff02ccadda13a72e4648
ganes asil <ganeshasil464@gmail.com>
2024-03-26 15:08:10 +0530

    forloop

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,33 +1,44 @@
 <template>
-    
-    <h2 v-if="show">If condition</h2>
-    <h2 v-else>Else condition</h2>
-    <br>
-    <br>
-    <button v-on:click="show=!show">show</button>
-    
+
+<ul>
+    <li v-for="name in user" :key="name">
+        {{ name.mail }}
+    </li>
+</ul>
+
+
 </template>
 
 <script>
 export default {
-    name:"HomeComponent",
-    data(){
-        return{
-         show:true
-           
-        }
-    },
-    methods:{
-        getData:function(){
-            console.log("values ",this.email,this.password)
+    name: "HomeComponent",
+    data() {
+        return {
+            technology:["java","python","sql"],
+            user:[
+                {
+                    name:"asil ganesh",
+                    mail:"ganesh@gmail.com"
+                },
+                {
+                    name:"veda",
+                    mail:"veda@gmail.com"
+                },
+                {
+                    name:"sainath",
+                    mail:"sainath@gmail.com"
+                }
+            ]
+
         }
     }
- 
+
+
 } 
 </script>
 
 <style scoped>
-h1{
-    color:orange
+h1 {
+    color: orange
 }
 </style>
